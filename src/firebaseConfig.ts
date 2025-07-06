ad4460314d34b148cf5e03760bb9daed9edb21d0
EducitoUwu <130678107+EducitoUwu@users.noreply.github.com>
2025-07-06 16:10:38 -0400

    credencales firebase

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDIAZ1_Wb_HmHzlDUqT0BepqKT9qqI2Dg",
-  authDomain: "clinic-hub-e56fd.firebaseapp.com",
-  projectId: "clinic-hub-e56fd",
-  storageBucket: "clinic-hub-e56fd.firebasestorage.app",
-  messagingSenderId: "970655458951",
-  appId: "1:970655458951:web:3ac3992aba7d3a9f87ba22",
-  measurementId: "G-WVT19DT4EZ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
   
 
